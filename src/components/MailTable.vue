b49d7b2228a0442997b00bbf2e006cb4a9da2900
Chuck Smith <chuck@eclecticsaddlebag.com>
2020-09-23 07:32:44 -0400

    Setup json-server

---
+++ src/components/MailTable.vue
@@ -0,0 +1,63 @@
+<template>
+  <table class="mail-table">
+    <tbody>
+    <tr v-for="email in unArchivedEmails"
+        :key="email.id"
+        :class="['clickable', email.read ? 'read' : '']"
+        @click="readEmail(email)">
+      <td><input type="checkbox" /></td>
+      <td>{{ email.from }}</td>
+      <td>
+        <p><strong>{{ email.subject }}</strong> - {{ email.body }}</p>
+      </td>
+      <td class="date">{{ format(new Date(email.sentAt), 'MMM do yyyy') }}</td>
+      <td>
+        <button @click="archiveEmail(email)">Archive</button>
+      </td>
+    </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+import { format } from 'date-fns';
+import axios from 'axios';
+
+export default {
+  async setup() {
+    let response = await axios.get('http://localhost:3000/emails')
+    let emails = response.data
+    return {
+      format,
+      emails
+    };
+  },
+  computed: {
+    sortedEmails() {
+      return this.emails.sort((e1, e2) => {
+        return e1.sentAt < e2.sentAt ? 1 : -1
+      })
+    },
+    unArchivedEmails() {
+      return this.sortedEmails.filter(e => !e.archived)
+    }
+  },
+  methods: {
+    readEmail(email) {
+      email.read = true
+      this.updateEmail(email)
+    },
+    archiveEmail(email) {
+      email.archived = true
+      this.updateEmail(email)
+    },
+    updateEmail(email) {
+      axios.put(`http://localhost:3000/emails/${email.id}`, email)
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
